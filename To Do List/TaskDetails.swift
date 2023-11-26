ea1fc3500bf3e6d96055567115f42c0d1be78629
Ethan Xu <ethanxucoder@gmail.com>
2023-11-25 21:35:28 -0500

    - Create working prototype

---
+++ To Do List/TaskDetails.swift
@@ -0,0 +1,18 @@
+//
+//  TaskDetails.swift
+//  To Do List
+//
+//  Created by Ethan Xu on 2023-11-25.
+//
+
+import SwiftUI
+
+struct TaskDetails: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskDetails()
+}
